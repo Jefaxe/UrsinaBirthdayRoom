f0523461fda982cc097335e42041819c2e5d6a7b
Jefaxe <73656286+Jefaxe@users.noreply.github.com>
2021-05-09 08:57:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.DEBUG)
 # Define a Voxel class.
 # By setting the parent to scene and the model to 'cube' it becomes a 3d button.
 
-table = Entity(position=(0, 2, 0), model='cube', scale=(2, 2, 2), color=color.brown, collider="cube")
+table = Entity(position=(0, 2, 0), model='cube', scale=(2, 2, 2), color=color.brown, collider="box")
 plate = Entity(position=(0, 3.01, 0), model='circle', color=color.white, scale=(1.5, 1.5),
                rotation=90)
 glass = Entity(position=(0.8, 3, 0.8), model="cube", color=color.orange, scale=(0.25, 0.8, 0.25))
@@ -32,6 +32,7 @@ photo = Entity(position=(0.8, 3.1, 0), model="plane", texture="birthday.png")
 
 class Voxel(Button):
     def __init__(self, position=(0, 0, 0)):
+        self.playing = False
         super().__init__(
             parent=scene,
             position=position,
@@ -41,19 +42,32 @@ class Voxel(Button):
             highlight_color=color.lime,
         )
 
+    def playBirthday(self):
+        if not self.playing:
+            self.playing = True
+            Audio("birthday.mp3")
+            time.sleep(1)
+
     def input(self, key):
+        intPos = ()
+        for g in player.position:
+            intPos += (int(g),)
         if self.hovered:
-            if key == "r":
-                logging.info(f"The block is at position {self.position}")
+            if key == "p":
+                if intPos == (-1, 1, 0):
+                    if not self.playing:
+                        self.playBirthday()
             if key == "m":
                 logging.info(f"You are at {player.position}")
+            if key == "r":
+                logging.info(f"The block is at position {self.position}")
             if key == 'left mouse down' or key == "'":
                 voxel = Voxel(position=self.position + mouse.normal)
             if key == 'right mouse down' or key == ";":
                 destroy(self)
 
 
-player = FirstPersonController(position=(-1.96327, 1.5, 0.223245), rotation_x=15)
+player = FirstPersonController(position=(-6.97408, 1.5, -6.26188))
 
 for y in range(DEPTH):
     for z in range(-MAPSIZE, MAPSIZE):
